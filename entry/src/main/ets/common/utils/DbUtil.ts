bffc0c09d1fd72be3ef78b370a34ca3f72749944
fusu <835145679@qq.com>
2024-04-01 21:39:17 +0800

    记录项

--- entry/src/main/ets/common/utils/DbUtil.ts
+++ entry/src/main/ets/common/utils/DbUtil.ts
@@ -41,10 +41,10 @@ class DbUtil {
     })
   }
 
-  insert(tableName: string, obj: any, columns: ColumnInfo[]){
-    return new  Promise((resolve, reject) =>{
+  insert(tableName: string, obj: any, columns: ColumnInfo[]) {
+    return new Promise((resolve, reject) => {
       // 1.构建新增数据
-      let value = this.buildValueBucket(obj,columns)
+      let value = this.buildValueBucket(obj, columns)
       // 2.新增
       this.rdbStore.insert(tableName, value, (err, id) => {
         if (err) {
@@ -58,11 +58,77 @@ class DbUtil {
     })
   }
 
-  buildValueBucket(obj: any, columns: ColumnInfo[]): relationalStore.ValuesBucket{
+  delete(predicates: relationalStore.RdbPredicates): Promise<number> {
+    return new Promise((resolve, reject) => {
+      this.rdbStore.delete(predicates, (err, rows) => {
+        if (err) {
+          Logger.error('删除失败！', JSON.stringify(err))
+          reject(err)
+        } else {
+          Logger.debug('删除成功！删除行数：', rows.toString())
+          resolve(rows)
+        }
+      })
+    })
+  }
+
+  queryForList<T>(predicates: relationalStore.RdbPredicates, columns: ColumnInfo[]): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+      this.rdbStore.query(predicates, columns.map(info => info.columnName), (err, result) => {
+        if (err) {
+          Logger.error('查询失败！', JSON.stringify(err))
+          reject(err)
+        } else {
+          Logger.debug('查询成功！查询行数：', result.rowCount.toString())
+          resolve(this.parseResultSet(result, columns))
+        }
+      })
+    })
+  }
+
+  parseResultSet<T>(result: relationalStore.ResultSet, columns: ColumnInfo[]): T[] {
+    // 1.声明最终返回的结果
+    let arr = []
+    // 2.判断是否有结果
+    if (result.rowCount <= 0) {
+      return arr
+    }
+    // 3.处理结果
+    while (!result.isAtLastRow) {
+      // 3.1.去下一行
+      result.goToNextRow()
+      // 3.2.解析这行数据，转为对象
+      let obj = {}
+      columns.forEach(info => {
+        let val = null
+        switch (info.type) {
+          case ColumnType.LONG:
+            val = result.getLong(result.getColumnIndex(info.columnName))
+            break
+          case ColumnType.DOUBLE:
+            val = result.getDouble(result.getColumnIndex(info.columnName))
+            break
+          case ColumnType.STRING:
+            val = result.getString(result.getColumnIndex(info.columnName))
+            break
+          case ColumnType.BLOB:
+            val = result.getBlob(result.getColumnIndex(info.columnName))
+            break
+        }
+        obj[info.name] = val
+      })
+      // 3.3.将对象填入结果数组
+      arr.push(obj)
+      Logger.debug('查询到数据：', JSON.stringify(obj))
+    }
+    return arr
+  }
+
+  buildValueBucket(obj: any, columns: ColumnInfo[]): relationalStore.ValuesBucket {
     let value = {}
-    columns.forEach(info =>{
+    columns.forEach(info => {
       let val = obj[info.name]
-      if(typeof val !== 'undefined') {
+      if (typeof val !== 'undefined') {
         value[info.columnName] = obj[info.name]
       }
     })
